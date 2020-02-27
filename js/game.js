fc38e9e65fdb022d5464075488c48b77d34503e1
Eduardo Luna <EduLuna@users.noreply.github.com>
2020-02-27 14:43:36 +0100

    .

--- js/game.js
+++ js/game.js
@@ -452,6 +452,32 @@ var levels = {
 				{type:"hero", name:"jelly2",x:80,y:405},
 				{type:"hero", name:"jelly1",x:140,y:405},
 			]
+		},
+		{   // Third level
+			foreground:'desert-foreground',
+			background:'clouds-background',
+			entities:[
+				{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
+				{type:"ground", name:"wood", x:185,y:390,width:30,height:80,isStatic:true},
+
+				{type:"block", name:"wood", x:820,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:720,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:620,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:670,y:317.5,width:100,height:25},
+				{type:"block", name:"glass", x:770,y:317.5,width:100,height:25},
+
+				{type:"block", name:"glass", x:670,y:255,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:770,y:255,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:720,y:192.5,width:100,height:25},
+
+				{type:"villain", name:"vaquero",x:715,y:155,calories:590},
+				{type:"villain", name:"zombie1",x:670,y:405,calories:420},
+				{type:"villain", name:"zombie2",x:765,y:400,calories:150},
+
+				{type:"hero", name:"jelly3",x:30,y:415},
+				{type:"hero", name:"jelly2",x:80,y:405},
+				{type:"hero", name:"jelly1",x:140,y:405},
+			]
 		}
 
 		
